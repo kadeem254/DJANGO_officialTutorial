d607067da1c40ca50c34193b2881355ba26c7e16
kadeem254 <kadeemalexander254@gmail.com>
2023-09-11 21:17:03 +0300

    feat: added polls apps models
    Added  models for Question and Choice to the models app, The question model holds the raised
    issue for a poll, while the choices represent a single option beloning to a question.
    Question model has also been enabled for editing within the admin section of django.
    This commit also contains the commits made

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+# Question Model - Holds a question that can be raised and can have multiple
+# choices which can be voted for.
+class Question( models.Model ):
+    question_text = models.CharField( max_length=255 )
+    pub_date = models.DateTimeField( "date published" )
+
+    def __str__(self):
+        return self.question_text
+    
+    # method to check if question was posted in the last 24 hrs.
+    def was_published_recently( self ):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# Choice Model - Holds a single choice to a question, can recieve votes.
+class Choice( models.Model ):
+    question = models.ForeignKey( Question, on_delete=models.CASCADE )
+    choice_text = models.CharField( max_length=255 )
+    votes = models.IntegerField( default=0 )
+
+    def __str__(self):
+        return self.choice_text
